9a253f4de4a6aa19345e9aee25d3c644bcccc6ce
Prathamesh-V-Kirad <79924908+Prathamesh-V-Kirad@users.noreply.github.com>
2025-03-15 13:58:07 +0530

    Update yt_comment_scrapper.py

--- yt_comment_scrapper.py
+++ yt_comment_scrapper.py
@@ -7,21 +7,17 @@ import requests
 from bs4 import BeautifulSoup
 
 def sanitize_filename(filename):
-    # Replace invalid characters with underscores
     return re.sub(r'[\\/*?:"<>|]', '_', filename)
 
 def get_youtube_title(video_url):
     try:
         # Make a GET request to the YouTube video URL
         response = requests.get(video_url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+        response.raise_for_status()
 
         # Parse the HTML content of the page
         soup = BeautifulSoup(response.text, 'html.parser')
-        # Find the title element
         title_element = soup.find('title')
-
-        # Extract the title text
         video_title = title_element.text.strip()
 
         return video_title
@@ -33,9 +29,8 @@ def extract_youtube_comments(video_url,video_title):
     driver = webdriver.Chrome()
 
     try:
-        # Open the video URL
         driver.get(video_url)
-        time.sleep(5)  # Waiting for the video to load
+        time.sleep(5)
         body = driver.find_element(By.TAG_NAME, 'body')
         for _ in range(20):
             body.send_keys(Keys.END)
@@ -60,10 +55,8 @@ def extract_youtube_comments(video_url,video_title):
         print(f"An error occurred: {e}")
 
     finally:
-        # Close the webdriver
         driver.quit()
 
-# Example usage
 video_url = "https://www.youtube.com/watch?v=LIWHz4g_n_w"
 video_title = get_youtube_title(video_url).split('-')[0]
 extract_youtube_comments(video_url,video_title)
